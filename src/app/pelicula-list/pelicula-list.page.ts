96e14ac0f1e687f4af36075a0d1cb13eab39a6d2
joskeiner urdaneta <joskeiner.simosa9@gmail.com>
2023-06-10 22:37:20 -0300

    se agrego una funcion para controlar el scroll

--- src/app/pelicula-list/pelicula-list.page.ts
+++ src/app/pelicula-list/pelicula-list.page.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { DbMovieService } from '../services/db-movie.service';
+import { IonContent } from '@ionic/angular';
+
+
+
 
 @Component({
   selector: 'app-pelicula-list',
@@ -7,11 +11,22 @@ import { DbMovieService } from '../services/db-movie.service';
   styleUrls: ['./pelicula-list.page.scss'],
 })
 export class PeliculaListPage implements OnInit {
+  // https://ionicframework.com/docs/api/content
+  // El signo de exclamación ! después de content es 
+  // una sintaxis de TypeScript llamada “definición de
+  //  propiedad no nula”. Se utiliza para indicar al compilador
+  //   que la propiedad content siempre tendrá un valor y nunca será nula o 
+  //   indefinida. Esto permite al compilador realizar comprobaciones más estrictas
+  //    y ayudar a prevenir errores en tiempo de ejecución
+  @ViewChild(IonContent)
+  content!: IonContent;
 
-  Page : number = 0;
+  Page: number =0;
   characters : any[] = [];
+
   constructor(
     private ApiMovieService: DbMovieService
+
   ) { }
 
   ngOnInit() {
@@ -19,14 +34,18 @@ export class PeliculaListPage implements OnInit {
     this.GetChacters();
 
   }
+  scrollToTop(){
+    this.content.scrollToTop(500);
+  }
+
 
   GetChacters(event?: any ){
     try{
       this.Page +=1 ;
     this.ApiMovieService.ListMovie(this.Page).subscribe(res => {
       this.characters.push(...res.results)
-      console.log(this.characters)
-      console.log(this.Page);
+      //console.log(this.characters)
+      //console.log(this.Page);
       if(event) event.target.complete();
     })
     }
@@ -34,13 +53,13 @@ export class PeliculaListPage implements OnInit {
       console.log(err);
       if (event) event.target.complete();
     }
-    
-
-
   }
+
 }
-// his.ApiMovieService.ListMovie(page)
-//     .subscribe( res =>{
-//       console.log(res);
-//       this.peliculas = res.results;
-//     
+
+
+
+
+
+
+
